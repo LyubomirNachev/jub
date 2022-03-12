52609715783f6cf3eb2b5a0f12d6a0d06525629e
amendip <34170667+amendip@users.noreply.github.com>
2022-03-12 03:00:27 +0200

    Над 100 реда изтрива последния

--- datawrite.php
+++ datawrite.php
@@ -14,8 +14,12 @@ $conn = mysqli_connect('localhost', 'root', '', '60 metra');
 $query="insert into `test 60 metra` values ("; 
 foreach($argv as $value)
 {
-  $query.="$value, ";
+  $query.="$value ";
 }
 $query.=")";
 mysqli_query($conn,$query);
+$nr = mysql_num_rows(mysqli_query($conn,"SELECT * FROM `test 60 metra`"));
+if($nr>100){
+mysqli_query($conn,"delete from `test 60 metra` order by id desc limit 1");
+}
 ?>

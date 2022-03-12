0d27d41aeb407afe86585171a3f5ec16a58480f1
boris7645 <61872427+boris7645@users.noreply.github.com>
2022-03-12 17:10:38 +0200

    popravka vnesena ot qsencho

--- datawrite.php
+++ datawrite.php
@@ -1,25 +1,15 @@
 <?php
-$colz=[
-"Methane(CH4)",
-"Benzene(C6H6)",
-"Smoke",
-"Carbon Monoxide(CO)",
-"Hydrogen(H2)",
-"Flammable gases",
-"Humidity",
-"Temperature",
-"Carbon Dioxide(CO2)"
-];
+""
 $conn = mysqli_connect('localhost', 'root', '', '60 metra');
-$query="insert into `test 60 metra` values ("; 
+$query="insert into `test 60 metra` (`Methane(CH4)`, `Benzene(C6H6)`, `Smoke`, `Carbon Monoxide(CO)`, `Hydrogen(H2)`, `Flammable gases`, `Humidity`, `Temperature`, `Carbon Dioxide(CO2)`) values ("; 
 foreach($argv as $value)
 {
-  $query.="$value ";
+  $query.="$value, ";
 }
 $query.=")";
 mysqli_query($conn,$query);
 $nr = mysql_num_rows(mysqli_query($conn,"SELECT * FROM `test 60 metra`"));
 if($nr>100){
-mysqli_query($conn,"delete from `test 60 metra` order by id desc limit 1");
+mysqli_query($conn,"delete from `test 60 metra` where id=1");
 }
 ?>
